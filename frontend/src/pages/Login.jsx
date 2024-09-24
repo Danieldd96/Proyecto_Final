183e5a009050e2433c228a4269c7c5929f6d12a3
Danieldd96 <danieldelgado.dd270@gmail.com>
2024-09-24 10:28:35 -0600

    Instalacion de paquetes y creacion de validacion para el login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,24 +1,41 @@
+import { useState } from "react"
 import { Link, Navigate } from "react-router-dom"
-
+import validation from "../utils/LoginValidation"
 
 
 const Login = ()=>{
+    const [values,setValues]=useState({
+        email:'',
+        password:''
+    })
+    const [errors,setErrors]=useState({})
+    const handleInput=(e)=>{
+        setValues(prev =>({...prev,[e.target.name]:[e.target.value]}))
+    }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        setErrors(validation(values))
+    }
+
     return(
     <div className="register" >
         <div className="wrapper" id="menu2">
             <div className="login" >
-                <form id="loginForm">
+                <form onSubmit={handleSubmit}>
                     <h1>Login</h1>
 
                         <div className="input-box">
                             <input type="text" placeholder="Correo Electronico" 
-                            required id="email" />      
+                            onChange={handleInput} name="email" />
+                            {errors.email && <span className="textoError">{errors.email} </span>}  
                         </div>
 
                         <div className="input-box">
                             <input type="password" 
                             placeholder="Contraseña" 
-                            required id="pass" />
+                            onChange={handleInput} name="password" />
+                            {errors.password && <span className="textoError">{errors.password} </span>}
                         </div>
 
                         <div className="ChangePass">
@@ -30,7 +47,7 @@ const Login = ()=>{
                             <p>No tienes una cuenta? <Link to="/registro" className="registerBtn">Register</Link></p>
                         </div>
 
-                        <button type="button"  className="btn">Login</button>
+                        <button type="submit"  className="btn">Login</button>
                 </form>
             </div>
         </div>
