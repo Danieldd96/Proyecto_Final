b488669f9707b50f6779b20422a17668d303f243
Danieldd96 <danieldelgado.dd270@gmail.com>
2024-09-26 10:16:05 -0600

    Re ordenamiento y cambios generales a todo el codigo de registro y login para mayor legibilidad, creacion de mensajes en ambas paginas al realizar acciones y asi dar una buena retroalimentacion junto con sus validaciones

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,92 +1,100 @@
-import { useState } from "react"
-import validation from "../utils/LoginValidation"
-import { useNavigate } from "react-router-dom"
-import { useContext, useEffect } from "react"
-import { Link } from "react-router-dom"
-import { Get } from "../hooks/Get"
-import { AuthContext } from "../contexts/AuthProvider"
+import { useState, useEffect, useContext } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { Get } from "../hooks/Get";
+import { AuthContext } from "../contexts/AuthProvider";
 
-const Login = ()=>{
-    const [values,setValues]=useState({
-        email:'',
-        password:''
-    })
-    const [errors,setErrors]=useState({})
-    const handleInput=(e)=>{
-        setValues(prev =>({...prev,[e.target.name]:[e.target.value]}))
-    }
+const Login = () => {
+  const navegar = useNavigate();
+  const [email, setEmail] = useState("");
+  const [clave, setClave] = useState("");
+  const { login } = useContext(AuthContext);
+  const [datos, setDatos] = useState([]);
+  const [mensaje, setMensaje] = useState("");
+  const [mensajeTipo, setMensajeTipo] = useState("");
+  const apiUrl = "http://127.0.0.1:8000/api/v2/user/usuarios/";
 
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        setEmail(e.target.value)
-        setClave(e.target.value)
-        setErrors(validation(values))
-    }
+  useEffect(() => {
+    obtener();
+  }, []);
 
-    const navegar = useNavigate();                 ///navegar sera el que se utilizara para pasar a otra pagina al completar una funcion
-    const [email, setEmail] = useState("");        ///Con este UseState obtendre el contenido del input en el cual se obtendra el email
-    const [clave, setClave] = useState("");        ///Con este UseState obtendre el contenido del input en el cual se obtendra el la contraseña
-    const {login} = useContext(AuthContext);       ///Con este UseContext verificare cuales rutas seran privadas
-    const [datos,setDatos]= useState("")
-    let apiUrl="http://127.0.0.1:8000/api/v2/user/usuarios/"      ///Api que contendra los datos de los usuarios que se hayan registrado previamente
-    useEffect(()=>{
-      obtener()
-    },[])
-    async function obtener() {
-      const data= await Get(apiUrl)
-       setDatos(data)
-    }
-    console.log(datos)
-    const Guardar = async () => {
-      if (!email==""&&!clave=="") {
-        const user=datos.find((user)=>user.mail_user==email)
-        console.log("Usuario encontrado",user)
-        if (user.contrasena==clave) {
-         localStorage.setItem("email",user.mail_user)
-         localStorage.setItem("idUsuario",user.id)
-         localStorage.setItem("usuario",user.user)
-         navegar('/home')
-         login()
-        }
-      } else {
-        alert('Usuario o contraseña incorrectos')
+  async function obtener() {
+      const data = await Get(apiUrl);
+      setDatos(data);
+  }
+
+  const Guardar = async () => {
+      setMensaje("");
+
+      if (email.trim() === "" || clave.trim() === "") {
+          setMensaje("Por favor, completa todos los campos.");
+          setMensajeTipo("error");
+          return;
       }
-    };  
 
-    return(
-    <div className="overlay" >
-        <div className="wrapper" id="menu2">
-            <div className="login" >
-                <form onSubmit={handleSubmit}>
+      if (email === clave) {
+          setMensaje("El correo y la contraseña no pueden ser iguales.");
+          setMensajeTipo("error");
+          return;
+      }
+
+      const user = datos.find((user) => user.mail_user === email);
+
+      if (!user) {
+          setMensaje("Usuario no encontrado.");
+          setMensajeTipo("error");
+          return;
+      }
+
+      if (user.contrasena !== clave) {
+          setMensaje("Contraseña incorrecta.");
+          setMensajeTipo("error");
+          return;
+      }
+      localStorage.setItem("email", user.mail_user);
+      localStorage.setItem("idUsuario", user.id);
+      localStorage.setItem("usuario", user.user);
+      setMensaje("Inicio de sesión exitoso.");
+      setMensajeTipo("success");
+      setTimeout(() => {
+          navegar('/home');
+          login();
+      }, 2000);
+  };
+
+  return (
+      <div className="overlay">
+          <div className="wrapper" id="menu2">
+              <div className="login">
+                  <form>
                     <h1>Login</h1>
 
-                        <div className="input-box">
-                            <input type="text" placeholder="Correo Electronico" 
-                            onChange={handleInput} name="email" />
-                            {errors.email && <span className="textoError">{errors.email} </span>}  
-                        </div>
+                    {mensaje && (
+                      <div className={`alert ${mensajeTipo}`}>
+                          {mensaje}
+                      </div>
+                    )}
+                    <div className="input-box">
+                      <input type="text"placeholder="Correo Electrónico"onChange={(e) => setEmail(e.target.value)}name="email"/>
+                    </div>
 
-                        <div className="input-box">
-                            <input type="password" 
-                            placeholder="Contraseña" 
-                            onChange={handleInput} name="password" />
-                            {errors.password && <span className="textoError">{errors.password} </span>}
-                        </div>
+                    <div className="input-box">
+                      <input type="password" placeholder="Contraseña" onChange={(e) => setClave(e.target.value)} name="password"/>
+                    </div>
 
-                        <div className="ChangePass">
-                            <a href="#">Olvidaste tu contraseña?</a>
-                        </div>
+                    <div className="ChangePass">
+                      <a href="#">Olvidaste tu contraseña?</a>
+                    </div>
 
+                    <div className="register-link">
+                      <p>No tienes una cuenta? <Link to="/registro" className="registerBtn">Register</Link></p>
+                    </div>
 
-                        <div className="register-link">
-                            <p>No tienes una cuenta? <Link to="/registro" className="registerBtn">Register</Link></p>
-                        </div>
-
-                        <button type="submit" onClick={Guardar} className="btn">Login</button>
-                </form>
-            </div>
-        </div>
-    </div>      
-    )
+                    <button type="button" onClick={Guardar} className="btn">Login</button>
+                  </form>
+              </div>
+          </div>
+      </div>
+    );
 }
-export default Login
+
+export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,85 +1,127 @@
-import { Link, useNavigate } from "react-router-dom"
-import { useState,useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { darDatos } from "../hooks/Post";
 import { Get } from "../hooks/Get";
 
+const Register = () => {
+    const navegar = useNavigate();
+    const [user, setUser] = useState("");
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+    const [datos, setDatos] = useState([]);
+    const [mensaje, setMensaje] = useState("");
+    const [mensajeTipo, setMensajeTipo] = useState("");
+    const [aceptarTerminos, setAceptarTerminos] = useState(false);
+    const apiUrl = "http://127.0.0.1:8000/api/v2/user/usuarios/";
 
-const Register = ()=>{
-   
-    const navegar = useNavigate('')   ///Este navegar me permitira trasladarme a otra pagina
-    ///Estos estados se utilizaran para contener los valores de sus respectivos inputs
-    const [user,setUser] = useState(""); 
-    const [email,setEmail] = useState(""); 
-    const [pass,setPass] = useState("");
-    const [datos,setDatos]= useState("")
-    ///Esta url es el api en el cual guardaremos los usuarios
-    let apiUrl="http://127.0.0.1:8000/api/v2/user/usuarios/"
-    useEffect(()=>{
-        obtener()
-      },[])
-      async function obtener() {
-        const data= await Get(apiUrl)
-         setDatos(data)
-      }
-    const Guardar = async()=>{     ///Esta funcion Guardar sera la que mandara el objetos al metodo Post
-       
-        const userExist=datos.find((acUser)=>acUser.email===email)
-        if (userExist) {
-            alert("Email ya registrado")
-        }else{
-        if (user.trim()===""||email.trim()===""||pass.trim()==="") { ///Este if verificara si los estados contienen vacios y si los contiene manda un alert que pedira que se coloque texto
-            //obtiene los datos del usuario
-            // valida si ya existe 
-            alert("Inserte texto por favor")
-          }else {                         ///Si no esta vacio se mandara el objeto let usuarios que tendra los estados especificos
-            console.log("Entrando");
-            let usuarios={
-                user:user,
-                mail_user:email,
-                contrasena:pass,
-            }
-            await darDatos(usuarios,apiUrl)    ///aqui se mandara el objeto al metodo post como parametro junto con el url de la api
-            setTimeout(() => {      ///Y se usara un setTimeout de un segundo que con el navegar me llevara a la pagina de /login
-                navegar('/')
-            }, 1000);
-          }
-        }
+    useEffect(() => {
+        obtener();
+    }, []);
+
+    async function obtener() {
+        const data = await Get(apiUrl);
+        setDatos(data);
     }
 
-    return(
-    <div className="register" >
-        <div className="wrapper">
-            <form id="registerForm">
-                <h1>Registro</h1>
+    const Guardar = async () => {
+        setMensaje("")
+        if (!aceptarTerminos) {
+            setMensaje("Debes aceptar los términos y condiciones.");
+            setMensajeTipo("error");
+            return;
+        }
 
-                <div className="input-box">
-                    <input type="text" id="name" placeholder="Usuario" 
-                    required onChange={(e)=>setUser(e.target.value)}/>
-                </div>
+        const userExist = datos.find((acUser) => acUser.mail_user === email);
+        if (userExist) {
+            setMensaje("Email ya registrado");
+            setMensajeTipo("error");
+            return;
+        }
 
-                <div className="input-box">
-                    <input type="email" id="email" placeholder="Correo Electronico" 
-                    required onChange={(e)=>setEmail(e.target.value)} />
-                </div>
+        if (user.trim() === "" || email.trim() === "" || pass.trim() === "") {
+            setMensaje("Inserte texto por favor");
+            setMensajeTipo("error");
+            return;
+        }
 
-                <div className="input-box">
-                    <input type="password" 
-                    placeholder="Contraseña" 
-                    required id="password" onChange={(e)=>setPass(e.target.value)}/>
-                </div>
-                
+        let usuarios = {
+            user: user,
+            mail_user: email,
+            contrasena: pass,
+        };
 
-                <div className="register-link">
-                    <p><input type="checkbox" name="check" id="check" />Estoy de acuerdo con los terminos y condiciones</p>
-                </div>
-                
-                <div className="register-link">
-                    <p>Ya tienes una cuenta? <Link to="/" className="registerBtn">Login</Link></p>
-                </div>
-                <button type="button" onClick={Guardar} className="btn" >Registrar</button>
-            </form>
+        await darDatos(usuarios, apiUrl);
+        setMensaje("Registro exitoso. Redirigiendo...");
+        setMensajeTipo("success");
+        setTimeout(() => {
+            navegar('/');
+        }, 2000); 
+    };
+
+    return (
+        <div className="register">
+            <div className="wrapper">
+                <form id="registerForm">
+                    <h1>Registro</h1>
+
+                    {mensaje && (
+                        <div className={`alert ${mensajeTipo}`}>
+                            {mensaje}
+                        </div>
+                    )}
+
+                    <div className="input-box">
+                        <input
+                            type="text"
+                            id="name"
+                            placeholder="Usuario"
+                            required
+                            onChange={(e) => setUser(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="input-box">
+                        <input
+                            type="email"
+                            id="email"
+                            placeholder="Correo Electrónico"
+                            required
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="input-box">
+                        <input
+                            type="password"
+                            placeholder="Contraseña"
+                            required
+                            id="password"
+                            onChange={(e) => setPass(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="register-link">
+                        <p>
+                            <input 
+                                type="checkbox" 
+                                name="check" 
+                                id="check" 
+                                onChange={(e) => setAceptarTerminos(e.target.checked)} 
+                            />
+                            Estoy de acuerdo con los términos y condiciones
+                        </p>
+                    </div>
+
+                    <div className="register-link">
+                        <p>
+                            Ya tienes una cuenta? <Link to="/" className="registerBtn">Login</Link>
+                        </p>
+                    </div>
+                    <button type="button" onClick={Guardar} className="btn">Registrar</button>
+                </form>
+            </div>
         </div>
-    </div>
-    )
+    );
 }
-export default Register
+
+export default Register;
