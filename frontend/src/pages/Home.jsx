ad2fc8cdbc9fc8d8fa4b16b5332d6ddb4398fb59
Danieldd96 <danieldelgado.dd270@gmail.com>
2024-09-28 16:39:06 -0600

    Creacion de poductos destacados de la pagina principal

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/navbar'
 import Carrusel from '../components/carrusel'
 import "../styles/home.css"
 import Casillas from './Casillas'
-import { Grid } from '@radix-ui/themes'
+import { Grid,Link } from '@radix-ui/themes'
 
 function Home() {
   return (
@@ -11,13 +11,33 @@ function Home() {
       <Navbar />
       <Carrusel/>
       <div className='home'>
-      <h1 style={{marginBottom:50,padding:20, borderBottomWidth:50}}>Productos Destacados</h1>
-      <Grid columns="4" width="auto" gap="4">
+      <div style={{padding:50, margin:0}}>
+      <h1 >Productos Destacados Bicicletas<Link style={{marginLeft:400,fontSize:20}}>Ver Todo</Link></h1>
+      <Grid columns="4" width="auto" gap="4" p={"9"}>
         <Casillas />
         <Casillas />
         <Casillas />
         <Casillas />
       </Grid>
+      </div>
+      <div style={{padding:50, margin:0}}>
+      <h1>Productos Destacados Piezas<Link style={{marginLeft:400,fontSize:20}}>Ver Todo</Link></h1>
+      <Grid columns="4" width="auto" gap="4" p={"9"}>
+        <Casillas />
+        <Casillas />
+        <Casillas />
+        <Casillas />
+      </Grid>
+      </div>
+      <div style={{padding:50, margin:0}}>
+      <h1>Productos Destacados Ropa<Link style={{marginLeft:400,fontSize:20}}>Ver Todo</Link></h1>
+      <Grid columns="4" width="auto" gap="4" p={"9"}>
+        <Casillas />
+        <Casillas />
+        <Casillas />
+        <Casillas />
+      </Grid>
+      </div>
       </div>
     </div>
   )
