bdd246437ea3d001c1d5386b5a01f000ca2b33e8
Danieldd96 <danieldelgado.dd270@gmail.com>
2024-09-29 16:17:10 -0600

    Creacion de tres funciones para el buscador de la navbar que seran para mostrarlo/ocultarlo y que pueda mostrar una alerta de la busqueda hecha con un state del valor actual del input

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/navbar.css';
 
 const Navbar = () => {
+    const [consulta, setConsulta] = useState('');
+    const [mostrarbusqueda, setmostrarbusqueda] = useState(false); // Estado para mostrar y ocultar input de búsqueda
+
+    const Mostrarbuscador = () => {
+        setmostrarbusqueda(!mostrarbusqueda);
+    };
+
+    const inputEnter = (event) => {
+        if (event.key === 'Enter') {
+            buscador();
+        }
+    };
+    const buscador = () => {
+        if (consulta.trim()) {
+            alert(`Buscando: ${consulta}`); 
+        }
+    };
     return (
         <nav className="navbar">
             <div className="navbarIzquierdo">
@@ -15,6 +32,19 @@ const Navbar = () => {
             </div>
 
             <div className="navbarDerecho">
+                <div className="search-container">
+                    <i className='bx bx-search search-icon' onClick={Mostrarbuscador}></i>
+                    {mostrarbusqueda && (
+                        <input
+                            type="text"
+                            className="search-input"
+                            value={consulta}
+                            onChange={(e) => setConsulta(e.target.value)}
+                            onKeyDown={inputEnter}
+                        />
+                    )}
+                </div>
+
                 <a href="#" className="cart-icon"><i className='bx bx-cart-alt' ></i></a>
                 <a href="#" className="face"><box-icon  type='logo' name='facebook-circle' color="rgb(55, 55, 203)"></box-icon></a>
                 <a href="#" className="insta"><box-icon type='logo' name='instagram-alt' color="#fff"></box-icon></a>
