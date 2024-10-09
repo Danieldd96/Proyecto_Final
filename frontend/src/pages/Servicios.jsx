67d72e8fd08f871b71f4d75e1639ef9751cba5e1
Danieldd96 <danieldelgado.dd270@gmail.com>
2024-10-09 13:35:39 -0600

    Creacion de la pagina de servicios con sus estilos

--- frontend/src/pages/Servicios.jsx
+++ frontend/src/pages/Servicios.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
 import Calendario from '../components/Calendario'
+import Navbar from '../components/navbar'
+import '../styles/Servicios.css'
+
 const Servicios = () => {
   return (
     <div>
+      <Navbar />
       <div>
-      <h1>Servicios</h1>
-        <h1>Recordatorios</h1>
-      <div style={{minWidth: 700,minHeight:200, padding:100}}>
-        <Calendario />
-
+        <div style={{padding: '10px 40px'}}>
+          <h1>Nuestros Servicios</h1>
+          <hr />
+        </div>
+      <div className='containerService'>
+        <div className="containerImg">
+          <img src="src/img/marzo.png" alt="" />
+          <h2>TUTORIALES DE MANTENIMIENTO</h2>
+        </div>
+        <div className="containerImg">
+          <img src="src/img/marzo.png" alt="" />
+          <h2>CALENDARIO DE MANTENIMIENTO</h2>
+        </div>
+        <div className="containerImg">
+          <img src="src/img/marzo.png" alt="" />
+          <h2>TALLER</h2>
+        </div>
+        <div className="containerImg">
+          <img src="src/img/marzo.png" alt="" />
+          <h2>CENTRO DE AYUDA</h2>
+        </div>
       </div>
       </div>
     </div>
