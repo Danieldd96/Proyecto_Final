2232d878022c5c58ab410c69a553f324fdace397
Danieldd96 <danieldelgado.dd270@gmail.com>
2024-10-16 15:44:12 -0600

    Correcciones en el home para usar productos del api y mostrar destacados de cada categoria

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,35 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import Carrusel from '../components/carrusel'
 import "../styles/home.css"
 import Casillas from '../components/Casillas'
-import { Grid,Link } from '@radix-ui/themes'
+import { Grid } from '@radix-ui/themes'
+import { Link } from 'react-router-dom'
 import ScrollButton from '../components/ScrollButton'
+import { Get } from '../hooks/Get'
 
 function Home() {
-  const productosBicicletas = [
-    { nombre: 'Bicicleta Eléctrica', precio: 1500, imagen: 'src/img/bicycle-1839005_1920.jpg' },
-    { nombre: 'Bicicleta de Montaña', precio: 1200, imagen: '' },
-    { nombre: 'Bicicleta de Ruta', precio: 900, imagen: '' },
-    { nombre: 'Bicicleta BMX', precio: 800, imagen: '' },
-  ];
+  const apiurl = 'http://127.0.0.1:8000/api/v3/producto/productos/';
+  const [productos, setProductos] = useState({});
+
+  const obtenerProductos = async () => {
+    const data = await Get(apiurl);
+
+    const categorias = ['Bicicletas', 'Nutrición', 'Ropa', 'Accesorios', 'Piezas'];
+    const productosFiltrados = {};
+
+    categorias.forEach(categoria => {
+      productosFiltrados[categoria] = data.filter(producto => producto.categoria === categoria);
+    });
+
+    setProductos(productosFiltrados);
+  };
+
+  useEffect(() => {
+    obtenerProductos();
+  }, []); 
 
   return (
     <div style={{border:"solid 1px",borderColor:"white"}}>
       <Navbar />
       <Carrusel/>
       <ScrollButton />
-      <div className='home'>
-      <div style={{padding:50, margin:0}}>
-      <h1 >Productos Destacados Bicicletas<Link style={{marginLeft:400,fontSize:20}}>Ver Todo</Link></h1>
-      <Grid columns="4" width="auto" gap="4" p={"9"}>
 
-        {productosBicicletas.map((producto, index) => (
-          <Casillas key={index} producto={producto} />
-        ))}
-        
-      </Grid>
-      </div>
+      <div className='home'>
+        <div style={{padding:50, margin:0}}>
+          {Object.keys(productos).map((categoria) => (
+            <div key={categoria}>
+
+              <h1>{`Productos Destacados - ${categoria}`} <Link to={`/${categoria.toLowerCase()}`} style={{ marginLeft: 400, fontSize: 20 }}>Ver Todo</Link></h1>
+              <Grid columns="4" width="auto" gap="4" p={"9"}>
+
+                {productos[categoria].map((producto, index) => (
+                  <Casillas key={index} producto={producto} />
+                ))}
+
+              </Grid>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
