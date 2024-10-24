776c97ed023b0b0b5fbefed1e4442d2e83b4bb3e
Danieldd96 <danieldelgado.dd270@gmail.com>
2024-10-24 11:21:51 -0600

    creacion de la vista para realizar busquedas desde la navbar

--- Productos/urls.py
+++ Productos/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import CategoriaViewSet, ProductosViewSet, ResenasViewSet
+from .views import BusquedaCategoriaView, CategoriaViewSet, ProductosViewSet, ResenasViewSet
 
 categoria_router = routers.DefaultRouter()
 categoria_router.register(r'categorias', CategoriaViewSet)
@@ -15,4 +15,6 @@ urlpatterns = [
     path('categoria/', include(categoria_router.urls)),
     path('producto/', include(productos_router.urls)),
     path('resenas/', include(resenas_router.urls)),
+    path('producto/buscar/<str:categoria>',BusquedaCategoriaView.as_view()),
+    
 ]   
--- Productos/views.py
+++ Productos/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializer import CategoriaSerializer,ProductosSerializer,ResenasSerializer
 from .models import Categoria,Productos,Resenas
-
+from rest_framework.generics import ListAPIView
 # Create your views here.
 
 class CategoriaViewSet(viewsets.ModelViewSet):
@@ -16,3 +16,11 @@ class ProductosViewSet(viewsets.ModelViewSet):
 class ResenasViewSet(viewsets.ModelViewSet):
     queryset = Resenas.objects.all()
     serializer_class = ResenasSerializer
+    
+class BusquedaCategoriaView(ListAPIView):
+    queryset = Productos.objects.all()
+    serializer_class = ProductosSerializer
+    lookup_field = 'categoria'
+    def get_queryset(self): 
+        producto_categoria = self.kwargs.get(self.lookup_field) 
+        return Productos.objects.filter(categoria=producto_categoria)
