f742357aa38ceb5879a7161ab15e0cb52ce3b94b
Danieldd96 <danieldelgado.dd270@gmail.com>
2024-11-04 08:26:29 -0600

    Creacion de los comentarios de la pagina accesorios

--- frontend/src/pages/Accesorios.jsx
+++ frontend/src/pages/Accesorios.jsx
@@ -1,6 +1,5 @@
 import React , { useState,useEffect } from 'react';
-import Navbar from '../components/navbar';
-import Casillas from '../components/Casillas';
+import Navbar from '../components/Navbar';
 import ScrollButton from '../components/ScrollButton';
 import '../styles/Bicicletas.css';
 import { Get } from '../hooks/Get';
@@ -8,21 +7,21 @@ import CasillasHorizontales from '../components/CasillasHorizontales';
 
 
 const Accesorios = () => {
-    const [filtrosAbiertos, setFiltrosAbiertos] = useState(false); 
-
-    const [productos, setProductos] = useState([]);
+    const [productos, setProductos] = useState([]);     /// Este estado se usa para mostrar los productos en la página
+    const api = 'http://127.0.0.1:8000/api/v3/producto/productos/';     /// Esta es la url de la api de la que se obtendran los productos
   
-    const obtenerProductos = async () => {
-        const data = await Get('http://127.0.0.1:8000/api/v3/producto/productos/');
+    const obtenerProductos = async () => {    /// Esta función se encarga de obtener los productos de la api
+        const data = await Get(api);
         const productosFiltrados = data.filter(producto => producto.categoria === 'Partes' || producto.categoria === 'Ropa' || producto.categoria === 'Nutrición');
+        //// Esta línea se encarga de filtrar los productos de acuerdo a la categoria que se seleccione
         setProductos(productosFiltrados);
     };
     
-    useEffect(() => {
+    useEffect(() => {      /// Esta función se encarga de llamar a la función de obtener productos
       obtenerProductos();
     }, []);
   
-    const Tipos = {
+    const Tipos = {      /// Esta es la variable que se utiliza para mostrar los diferentes tipos de productos
       Partes:'src/img/bicicleta-electrica.jpg',
       Ropa:'src/img/mountain-bike.jpg',
       Nutricion:'src/img/road-bike.jpg',
@@ -31,46 +30,35 @@ const Accesorios = () => {
   return (
     <div>
         <Navbar />
-
         <ScrollButton />
-
+        {/* El scroll button se encarga de desplazarse a la página de acuerdo a donde se esté scrollando */}
         <div className="container">
 
-        <div className={`sidebar ${filtrosAbiertos ? 'sidebar-open' : ''}`}>
-            <h2>Productos</h2>
-            <div className="scrollable-filters">
-            <h2>Filtros</h2>
-            <h2>filtro 1</h2>
-            <h2>filtro 2</h2>
-            <h2>filtro 3</h2>
-            </div>
-        </div>
-
-        <div className="content">
-            <div className="image-row">
-
-            {Object.entries(Tipos).map(([tipo, imagen], index) => (
-            <div key={index}>
-                <img src={imagen} alt={`Tipo ${tipo}`} />
-                <h2>{tipo}</h2>
-            </div>
-            ))}
-            </div>
-            <div>
-            
-            </div>
-            <h1 style={{borderBottom:"solid 5px #8b0000",paddingBottom:10,paddingLeft:20}}>Productos</h1>
-        <button className="filter-button" onClick={() => setFiltrosAbiertos(!filtrosAbiertos)}>
-            {filtrosAbiertos ? 'Cerrar Filtros' : 'Abrir Filtros'}
-        </button>
-            <div className="product-container">
-            {productos.map((producto, index) => (
-                <div key={index} className="product-card">
-                <CasillasHorizontales producto={producto} />
+            <div className="content">
+                <div className="image-row">
+                    {/* Esta es la imagen que se muestra en la parte superior de la página */}
+                    {Object.entries(Tipos).map(([tipo, imagen], index) => (
+                        // El object entries se utiliza para iterar sobre el objeto Tipos
+                        <div key={index}>
+                            <img src={imagen} alt={`Tipo ${tipo}`} />
+                            <h2>{tipo}</h2>
+                        </div>
+                    ))}
                 </div>
-            ))}
+
+                <h1 style={{borderBottom:"solid 5px #8b0000",paddingBottom:10,paddingLeft:20}}>Productos</h1>
+
+                <div className="product-container">
+                    {/* Esta es la sección que se encarga de mostrar los diferentes productos por medio del map */}
+                    {productos.map((producto, index) => (
+                        <div key={index} className="product-card">
+                            <CasillasHorizontales producto={producto} />
+                            {/* CasillasHorizontales es el componente que se encarga de mostrar las diferentes opciones de cada producto */}
+                        </div>
+                    ))}
+                </div>
+
             </div>
-        </div>
         </div>
     </div>
   )
