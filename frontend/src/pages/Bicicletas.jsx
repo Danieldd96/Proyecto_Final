43ee7d84d53c033f4163468a3cbd4ba303f7b909
Danieldd96 <danieldelgado.dd270@gmail.com>
2024-11-04 08:54:01 -0600

    Creacion de los comentarios de la pagina bicicletas

--- frontend/src/pages/Bicicletas.jsx
+++ frontend/src/pages/Bicicletas.jsx
@@ -1,6 +1,5 @@
 import React , { useState,useEffect } from 'react';
 import Navbar from '../components/navbar';
-import Casillas from '../components/Casillas';
 import ScrollButton from '../components/ScrollButton';
 import '../styles/Bicicletas.css';
 import { Get } from '../hooks/Get';
@@ -8,21 +7,23 @@ import CasillasHorizontales from '../components/CasillasHorizontales';
 
 
 const Bicicletas = () => {
-  const [filtrosAbiertos, setFiltrosAbiertos] = useState(false); 
-
-  const [productos, setProductos] = useState([]);
+  const [productos, setProductos] = useState([]);       ///Const para guardar los productos
+  const api = 'http://127.0.0.1:8000/api/v3/producto/productos/';
 
   const obtenerProductos = async () => {
-      const data = await Get('http://127.0.0.1:8000/api/v3/producto/productos/');
+      const data = await Get(api);
       const productosFiltrados = data.filter(producto => producto.categoria === 'Bicicletas');
+      ///Filtro para solo mostrar bicicletas
       setProductos(productosFiltrados);
+      ///Asignar los productos a la variable
   };
   
   useEffect(() => {
+    // Obtener los productos al cargar la página
     obtenerProductos();
   }, []);
 
-  const Tipos = {
+  const Tipos = {   //// Objeto para almacenar los tipos de bicicletas
     Electrica:'src/img/bicicleta-electrica.jpg',
     Montaña:'src/img/mountain-bike.jpg',
     Ruta:'src/img/road-bike.jpg',
@@ -38,40 +39,29 @@ const Bicicletas = () => {
 
       <div className="container">
 
-        <div className={`sidebar ${filtrosAbiertos ? 'sidebar-open' : ''}`}>
-          <h2>BICICLETAS</h2>
-          <div className="scrollable-filters">
-            <h2>Filtros</h2>
-            <h2>filtro 1</h2>
-            <h2>filtro 2</h2>
-            <h2>filtro 3</h2>
-          </div>
-        </div>
-
         <div className="content">
           <div className="image-row">
 
-          {Object.entries(Tipos).map(([tipo, imagen], index) => (
-            <div key={index}>
-              <img src={imagen} alt={`Tipo ${tipo}`} />
-                <h2>{tipo}</h2>
-            </div>
-          ))}
+            {Object.entries(Tipos).map(([tipo, imagen], index) => (
+              //El objeto Tipos es un objeto con las imagenes de los tipos de bicicletas
+              <div key={index}>
+                <img src={imagen} alt={`Tipo ${tipo}`} />
+                  <h2>{tipo}</h2>
+              </div>
+            ))}
           </div>
           <div>
             
           </div>
-          <h1 style={{borderBottom:"solid 5px #8b0000",paddingBottom:10,paddingLeft:20}}>Bicicletas</h1>
-        <button className="filter-button" onClick={() => setFiltrosAbiertos(!filtrosAbiertos)}>
-          {filtrosAbiertos ? 'Cerrar Filtros' : 'Abrir Filtros'}
-        </button>
-          <div className="product-container">
-            {productos.map((producto, index) => (
-              <div key={index} className="product-card">
-                <CasillasHorizontales producto={producto} />
-              </div>
-            ))}
-          </div>
+            <h1 style={{borderBottom:"solid 5px #8b0000",paddingBottom:10,paddingLeft:20}}>Bicicletas</h1>
+            <div className="product-container">
+              {productos.map((producto, index) => (
+                //La constante productos es un array con los productos el cual con el map pasamos al componente casillasHorizontales
+                <div key={index} className="product-card">
+                  <CasillasHorizontales producto={producto} />
+                </div>
+              ))}
+            </div>
         </div>
       </div>
     </div>
